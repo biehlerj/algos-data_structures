74655d4835d1e749f7191027630a8fc0f857742d
Jacob Biehler <biehlerj@gmail.com>
2018-10-07 11:15:07 -0700

    Adding error handling

--- sorting_algos/python/bubble_sort.py
+++ sorting_algos/python/bubble_sort.py
@@ -2,21 +2,23 @@
 '''
 Writing the Bubble Sort algorithm in Python
 '''
+from sys import exit
 
 def bubbleSort(array, size):
     '''
     Sorting an array of integers in ascending order using the Bubble sort algorithm
     '''
+    if array is None:
+        raise TypeError("No values in the list")
+    
+    if size == 0:
+        raise AttributeError("Size cannot be zero")
+    
+    if size == 1:
+        print("Array is already sorted")
+        exit -1
+
     for i in range(size):
         for j in range(0, size-i-1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
-
-# Code to test if bubble sort is working
-arr = [64, 34, 25, 12, 22, 11, 90]
- 
-bubbleSort(arr, len(arr))
- 
-print ("Sorted array is:")
-for i in range(len(arr)):
-    print ("%d" %arr[i]),
